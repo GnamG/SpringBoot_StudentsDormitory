da60160b1c83570800ace818661127a8bf18552f
zly <2998701715@qq.com>
2022-05-04 19:52:49 +0800

    学生管理接口完成

---
+++ src/main/java/com/fc/controller/buildingController.java
@@ -0,0 +1,9 @@
+package com.fc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class buildingController {
+
+}

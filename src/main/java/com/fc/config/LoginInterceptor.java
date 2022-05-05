87e64441a99f5b5a388bdb211548f1e8f618987a
zzx <635702657@q.com>
2022-05-05 15:10:28 +0800

    拦截器

---
+++ src/main/java/com/fc/config/LoginInterceptor.java
@@ -0,0 +1,34 @@
+package com.fc.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+// 拦截器
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        Object user = request.getSession().getAttribute("user");
+        Object type = request.getSession().getAttribute("userType");
+        if (user!=null&&type!=null){
+            return true;
+        }
+        response.sendRedirect(request.getContextPath()+"/login.html");
+
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
